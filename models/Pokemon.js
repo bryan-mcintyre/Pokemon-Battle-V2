3690ec88984b43294bb8ce181cd1060c0f76b8e3
Aleksandr <keytolife94@gmail.com>
2024-08-03 16:42:35 -0500

    Added model pokemon

---
+++ models/Pokemon.js
@@ -0,0 +1,109 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const PokemonLevel = require('./PokemonLevel');
+
+class Pokemon extends Model { }
+
+Pokemon.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        picture: {
+            type: DataTypes.TEXT,
+        },
+        alive: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true,
+        },
+        favorite: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: false,
+        },
+        level: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1,
+            },
+            defaultValue: 1,
+        },
+        experience: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+            },
+            defaultValue: 0,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        pokemon_level_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'pokemon_level',
+                key: 'id',
+            }
+        },
+    },
+    {
+        hooks: {
+            beforeSave: async (pokemon) => {
+
+                // get max lvl from pokemonLevel model
+                const maxLevel = await PokemonLevel.max('level');
+
+                // get current level pokemon
+                const currentLevel = pokemon.level;
+
+                // check current lvl >= max level
+                if (currentLevel >= maxLevel) {
+
+                    // if yes, then pokemon.exp = max value experience
+                    pokemon.experience = await PokemonLevel.findOne({
+                        where: { level: currentLevel }
+                    }).then(level => level.experience);
+
+                    return;
+                }
+
+                // get next level from pokemon lvl
+                const nextLevel = await PokemonLevel.findOne({
+                    where: {
+                        level: currentLevel + 1,
+                    },
+                });
+
+                // check we have nextLevel and experience pokemon > nextLevel experience
+                if (nextLevel && pokemon.experience >= nextLevel.experience) {
+
+                    pokemon.level = nextLevel.level;
+                    if (pokemon.level === maxLevel) {
+                        pokemon.experience = nextLevel.experience;
+                    }
+                }
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'pokemon',
+    }
+);
+
+module.exports = Pokemon;
