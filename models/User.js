06f9d7bb1f9f76d14870da882796cfafae8ecac8
Aleksandr <keytolife94@gmail.com>
2024-08-05 18:51:18 -0500

    added unique for name

--- models/User.js
+++ models/User.js
@@ -20,6 +20,7 @@ User.init(
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
         },
         password: {
             type: DataTypes.STRING,
