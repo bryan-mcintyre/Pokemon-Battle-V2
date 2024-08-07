7eadf92f262c8f2efe4c941d4f4dc1d2c877f770
Leena <leenacruz@gmail.com>
2024-08-06 19:04:14 -0700

    Backpack and store handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,56 @@
 const router = require('express').Router();
+const {Pokemon, PokemonStats} = require('../models');
 
+//Get Home route
 router.get('/', (req,res) =>{
     console.log("TEXT")
     res.render('home');
 } );
+
+router.get('/battle', async (req, res) => {
+    // What info (model and references) do I need to get?
+    // attack, hp, 
+//     if(req.session.loggedIn) {
+//     try {
+//         const BlogPostData = await BlogPost.findAll({
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['username'],
+//                 },
+//             ],
+//             where: {
+//                 user_id: req.session.user_id
+//             }
+//         });
+//         console.log(req.session.user_id)
+//         const blogPosts = BlogPostData.map((blogpost) => blogpost.get({ plain: true }));
+//        console.log(blogPosts);
+     
+//         res.render('dashboard', { 
+//             blogPosts,
+//             loggedIn: req.session.loggedIn
+//          });
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     } 
+// }else{
+//     res.redirect("/login");
+// }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
