e92b72c1128cbedd79a3c21e67700b0f5acb7d89
Leena <leenacruz@gmail.com>
2024-08-06 17:54:46 -0700

    Controllers - Home-Routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,10 @@
 const router = require('express').Router();
 // Import the index.js from 'api' folder
 const apiRoutes = require('./api');
+//Import home routes
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 // When a request is made to the /api route, it will be directed to the index.js in the 'api' folder.
 router.use('/api', apiRoutes);
 
