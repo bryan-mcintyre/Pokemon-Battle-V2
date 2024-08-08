98bdff51f48e201591ebe1e2f953223002eb46b3
Leena <leenacruz@gmail.com>
2024-08-07 18:54:54 -0700

    Erase unused code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,39 +30,6 @@ router.get('/', (req,res) =>{
 } );
 
 router.get('/battle', async (req, res) => {
-    // What info (model and references) do I need to get?
-    // attack, hp, 
-//     if(req.session.loggedIn) {
-//     try {
-//         const BlogPostData = await BlogPost.findAll({
-//             include: [
-//                 {
-//                     model: User,
-//                     attributes: ['username'],
-//                 },
-//             ],
-//             where: {
-//                 user_id: req.session.user_id
-//             }
-//         });
-//         console.log(req.session.user_id)
-//         const blogPosts = BlogPostData.map((blogpost) => blogpost.get({ plain: true }));
-//        console.log(blogPosts);
-     
-//         res.render('dashboard', { 
-//             blogPosts,
-//             loggedIn: req.session.loggedIn
-//          });
-
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     } 
-// }else{
-//     res.redirect("/login");
-// }
-
-
 res.render('battle');
 });
 //Backpack route
