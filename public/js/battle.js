84ed190271083d2711b2ffc3070911e7eb1ff28f
Colin Taaffe <85810714+BurnerTV@users.noreply.github.com>
2024-08-11 01:05:52 -0500

    commit

--- public/js/battle.js
+++ public/js/battle.js
@@ -45,7 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
             console.log(response);
 
-            // Check if the response is ok, then redirect
             if (response.ok) {
                 window.location.href = '/battle/startBattle';
             } else {
@@ -55,26 +54,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// const { Pokemon } = require("../../models");
-// const BattlePokemon = require("../../utils/BattlePokemon");
-// const { fetchRandomPokemon } = require("../../utils/pokemonFetch");
-
-
-// const pokemonData = await Pokemon.findByPk(1);
-// const enemyPokemondata = fetchRandomPokemon();
-
-// const pokemon = new BattlePokemon(pokemonData);
-// const enemyPokemon = new BattlePokemon(enemyPokemondata);
-
-// enemyPokemon.balanceStats(pokemon.level);
-
-// if (pokemon.speed > enemyPokemon.speed) {
-//     pokemon.attack(enemyPokemon)
-// } else {
-//     enemyPokemon.attack(pokemon);
-// }
-
-
-
-
-// pokemon.attack(enemyPokemon)
