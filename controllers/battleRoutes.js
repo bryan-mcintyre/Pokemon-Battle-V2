90ebf01ac85c0f58e91ad3233227f9dd35b27197
Aleksandr <keytolife94@gmail.com>
2024-08-10 19:39:07 -0500

    Change logic post for current user, save pokemon, create battle post, for render start-battle, added example start-battle

--- controllers/battleRoutes.js
+++ controllers/battleRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { withAuth } = require('../utils/auth');
-const { User, Pokemon, PokemonStats, PokemonAbility, Ability } = require('../models');
+const { fetchPokemonByName } = require('../utils/pokemonFetch')
+const { User, Pokemon, PokemonStats, PokemonAbility, Ability, PokemonLevel } = require('../models');
 
 // TODO: Add auth check and save session
 
@@ -31,5 +32,47 @@ router.get('/', withAuth, async (req, res) => {
     }
 });
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const userPokemonData = await Pokemon.findOne({
+            where: {
+                id: req.body.pokemon_id,
+                user_id: req.session.user_id
+            },
+            include: [
+                { model: PokemonStats },
+                { model: Ability, through: PokemonAbility }
+            ],
+        });
+        //get Battledata
+        const userPokemon = await userPokemonData.getBattleData();
+
+        // fetch pokemon enemy
+        const enemyPokemon = await fetchPokemonByName(req.body.opponent_pokemon);
+
+        // get all data from ability
+        const abilitiesData = await Ability.findAll();
+
+        //for take random ability
+        const randomIndex = Math.floor(Math.random() * abilitiesData.length);
+
+        // create random ability for enemy
+        const randomAbility = abilitiesData[randomIndex];
+
+        // add enemyPokemon random ability
+        enemyPokemon.abilities = [randomAbility.dataValues];
+
+        //get lvlData
+        const levelData = await PokemonLevel.findAll({ raw: true });
+
+        res.render('start-battle', {
+            userPokemon: userPokemon,
+            enemyPokemon: enemyPokemon,
+            levelData: levelData
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
 
 module.exports = router;
